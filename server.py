19d533273d8b308989a98ac14ea3f1eacfb31c55
Andy Thanh <tanthanh2285@gmail.com>
2024-08-04 13:35:40 +0000

    fix server py

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ def receive_alert():
         # Process the received data (e.g., print it, store it, etc.)
         print(f"Latency to Webex: {data['result']['AverageLatency']}ms")
         totalParticipant=get_total_participants()
-        if int(totalParticipant)>0 and int(latency)>100:
+        if int(totalParticipant)>0:
             bw=int(totalParticipant)*1000
             remaining_bw=10-int(totalParticipant)*1
             print(f"Latency to Webex is {latency}m, number of active webex sessions is {totalParticipant}, police bandwidth to {remaining_bw}m")
@@ -30,17 +30,7 @@ def receive_alert():
             f'service-policy output  dynamic'
         ]
             send_commands(cmd=cmd)
-        elif int(totalParticipant)>0 and (int(latency)<10):
-            print(f'has {totalParticipant} active Webex sessions but latency is good {latency}ms, no need to update QoS configuration')
-            cmd= [
-                f'no policy-map dynamic',
-                f'policy-map dynamic',
-                f' class class-default',
-                f'  fair-queue',
-                f'interface gi2',
-                f'service-policy output  dynamic'
-            ]
-            send_commands(cmd=cmd)
+        
         elif int(totalParticipant)==0 and int(latency)<60:
             print('no active Webex session and latency is acceptable (<60), no need to update QoS configuration')
             cmd= [
