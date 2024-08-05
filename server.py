29a5c97acd31973cc88d913af2b94e68779c3e09
Andy Thanh <tanthanh2285@gmail.com>
2024-08-05 07:06:31 +0000

    fine tune server

--- server.py
+++ server.py
@@ -11,6 +11,7 @@ app = Flask(__name__)
 def receive_alert():
     try:
         data = request.json
+        print(data)
         if not data:
             return jsonify({"error": "Invalid data"}), 400
         
@@ -21,26 +22,28 @@ def receive_alert():
 
         
         search_name="avgReceive"
-        print(search_name)
+        #print(search_name)
         rx_bw=fetch_saved_search_results(saved_search_name=search_name)
-        #print(f'RX BW status: {rx_bw}')
-        if int(totalParticipant)>0 and int(rx_bw)>9000:
-            print(f'Bandwidth is congested: {rx_bw}kbps')
-            bw=int(totalParticipant)*1000
-            remaining_bw=10-int(totalParticipant)*1
-            print(f"Latency to Webex is {latency}m, number of active webex sessions is {totalParticipant}, police bandwidth to {remaining_bw}m")
-            cmd = [
-            f'no policy-map dynamic',   
-            f'policy-map dynamic',
-            f' class WEBEX',
-            f'  priority {str(bw)}',
-            f' class class-default',
-            f'  police {remaining_bw}m',
-            f'interface gi2',
-            f'service-policy output  dynamic'
-        ]
-            send_commands(cmd=cmd)
-        
+        print(f'downstream bw status: {rx_bw["avgRx"]}kbps')
+        if int(totalParticipant)>0 and int(latency)>100:
+            if int(rx_bw)>9000:
+                print(f'Bandwidth is congested: {rx_bw}kbps')
+                bw=int(totalParticipant)*1000
+                remaining_bw=10-int(totalParticipant)*1
+                print(f"Latency to Webex is {latency}m, number of active webex sessions is {totalParticipant}, police bandwidth to {remaining_bw}m")
+                cmd = [
+                f'no policy-map dynamic',   
+                f'policy-map dynamic',
+                f' class WEBEX',
+                f'  priority {str(bw)}',
+                f' class class-default',
+                f'  police {remaining_bw}m',
+                f'interface gi2',
+                f'service-policy output  dynamic'
+            ]
+                send_commands(cmd=cmd)
+            else:
+                print('latency is high but not because of downstream congestion')
         elif int(totalParticipant)==0 and int(latency)<60:
             print('no active Webex session and latency is acceptable (<60), no need to update QoS configuration')
             cmd= [
