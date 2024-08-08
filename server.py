d1eac35bfb05eb684c1cd78e5399f76206ab29f0
Andy Thanh <tanthanh2285@gmail.com>
2024-08-08 20:39:00 +0700

    fix type error 1

--- server.py
+++ server.py
@@ -25,18 +25,18 @@ def receive_alert():
         search_name="avgReceive"
         #print(search_name)
         rx_bw=fetch_saved_search_results(saved_search_name=search_name)
-        remaining_bw=10000-float(rx_bw)
+        #remaining_bw=10000-float(rx_bw)
         data_bw=10-int(totalParticipant)*1
         print(f'downstream used bandwidth: {rx_bw["avgRx"]}kbps')
         print(f'required bandwidth for Webex: {int(totalParticipant)*1}000kbps')
-        print(f'available bandwidth: {remaining_bw}kbps')
+        #print(f'available bandwidth: {remaining_bw}kbps')
 
 
             
         if int(totalParticipant)>0:     
-                if int(latency)>100 or remaining_bw<int(totalParticipant)*1000:               
+                if int(latency)>100:             
                     bw=int(totalParticipant)*1000
-                    print(f"Latency to Webex is {latency}m which is high or remaining bandwidth is not enough for the number of active webex sessions {totalParticipant}, police data bandwidth to {remaining_bw}m")
+                    print(f"Latency to Webex is {latency}m which is high or remaining bandwidth is not enough for the number of active webex sessions {totalParticipant}, police data bandwidth to {data_bw}m")
                     cmd = [
                     f'no policy-map dynamic',   
                     f'policy-map dynamic',
