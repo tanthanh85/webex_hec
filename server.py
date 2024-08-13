b1e45327f25a2fde8464a2d2208ca4b2603a7251
Andy Thanh <tanthanh2285@gmail.com>
2024-08-13 13:35:25 +0000

    fine tune server

--- server.py
+++ server.py
@@ -47,8 +47,8 @@ def receive_alert():
                 ]
                 send_commands(cmd=cmd)
                      
-        elif int(totalParticipant)==0 and int(latency)<60:
-            print('no active Webex session and latency is acceptable (<60), no need to update QoS configuration')
+        elif int(totalParticipant)==0 and int(latency)<80:
+            print('no active Webex session and latency is acceptable (<80), no need to update QoS configuration')
             cmd= [
                 f'no policy-map dynamic',
                 f'policy-map dynamic',
@@ -58,8 +58,8 @@ def receive_alert():
                 f'service-policy output  dynamic'
             ]
             send_commands(cmd=cmd)
-        elif int(totalParticipant)==0 and int(latency)>60:
-            print('has no active Webex session but latency is greater than 60ms, please consider to use AI to estimate the no of webex sessions')
+        elif int(totalParticipant)==0 and int(latency)>=80:
+            print('has no active Webex session but latency is greater than 80ms, please consider to use AI to estimate the no of webex sessions')
         #     cmd = [
         #     f'policy-map dynamic',
         #     f' class WEBEX',
@@ -82,10 +82,34 @@ def meeting():
         #print(data)
         if not data:
             return jsonify({"error": "Invalid data"}), 400
-        totalparticipant=data['result']['totalparticipant']
-        print(totalparticipant)
+        totalParticipant=data['result']['TotalParticipant']
+        #print(totalparticipant)
+        print(f'There is {totalParticipant} live Webex participant in the office, applying QoS now')
+
+        search_name="avgReceive"
+        #print(search_name)
+        rx_bw=fetch_saved_search_results(saved_search_name=search_name)
+        remaining_bw=10000-float(rx_bw["avgRx"])
+        data_bw=10-int(totalParticipant)*1-3
+        webex_bw=int(totalParticipant)*1000
+        print(f'downstream used bandwidth: {rx_bw["avgRx"]}kbps')
+        print(f'required bandwidth for Webex: {str(webex_bw)}kbps')
+        print(f'available bandwidth: {remaining_bw}kbps')
+
+        cmd = [
+                f'no policy-map dynamic',   
+                f'policy-map dynamic',
+                f' class WEBEX',
+                f'  priority {str(webex_bw)}',
+                f' class class-default',
+                f'  police {data_bw}m',
+                f'interface gi2',
+                f'service-policy output  dynamic'
+                ]
+        send_commands(cmd=cmd)
 
 
+        return jsonify({"message": "Alert received"}), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
